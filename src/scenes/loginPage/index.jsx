bf75a1e638921c35699539261d9a5d513f7c91c0
칼있으마 <bok-yeol.jung@doosan.com>
2024-01-29 16:40:57 +0900

    2:47:07까지 했음 일단 로긴페이지랑 등록 페이지를 만들었지만 로긴 페이지만 열리는거 확인했음

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import {Box, Typography, useTheme, useMediaQuery} from '@mui/material';
+import Form from "./Form";
 
 const LoginPage = () => {
-    return (<div>loginpage</div>)
+    const theme = useTheme();
+    const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
+    return <Box>
+        <Box 
+            width="100%" 
+            backgroundColor={theme.palette.background.alt} 
+            p="1rem 6%"
+            textAlign="center"
+        >
+            <Typography 
+                fontWeight="bold"
+                fontSize="32px"
+                color="primary"
+            >
+                나는 천재
+            </Typography>            
+        </Box>
+
+        <Box
+            width={isNonMobileScreen ? "50%" : "93%"}
+            p="2rem"
+            m="2rem auto"
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" variant="h5" sx={{mb:"1.5rem"}}>
+                여기 온거슬 환영한당깨 ... 쇼셜미디어를 거시기 하랑깨
+            </Typography>
+            <Form />
+        </Box>
+    </Box>;
 }
 
 export default LoginPage;
